02d0ad09ac98c8890dc3993f37dd2955e9099c82
Abdulmumin Akinde <lanreywaju97@gmail.com>
2024-03-28 17:50:19 +0100

    Added LeafNode class and methods with tests

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -18,3 +18,15 @@ class HTMLNode:
         return (
             f"HTMLNode{self.tag}, {self.value}, children: {self.children}, {self.props}"
         )
+
+
+class LeafNode(HTMLNode):
+    def __init__(self, value, tag=None, props=None):
+        super().__init__(value, tag, None, props)
+
+    def to_html(self):
+        if self.value is None:
+            raise ValueError("Value cannot be empty")
+        if self.tag is None:
+            return self.value
+        return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,6 +1,6 @@
 from unittest import TestCase
 
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, LeafNode
 
 
 class TestHtmlNode(TestCase):
@@ -14,3 +14,33 @@ class TestHtmlNode(TestCase):
     def test_props_to_html_works_when_no_props(self):
         node = HTMLNode("div", "Hello, World!", children=None, props=None)
         self.assertEqual(node.props_to_html(), "")
+
+
+class TestLeafNode(TestCase):
+
+    def test_leaf_node_raises_error_when_value_is_none(self):
+        with self.assertRaises(ValueError):
+            LeafNode(None).to_html()
+
+    def test_to_html_works(self):
+        div_node = LeafNode(
+            "div", "Hello, World!", props={"class": "test", "id": "test"}
+        )
+        link_node = LeafNode(
+            "a", "Click me!", props={"href": "https://www.example.com"}
+        )
+        self.assertNotEqual(div_node.to_html(), "Hello, World!")
+        self.assertEqual(
+            div_node.to_html(), '<div class="test" id="test">Hello, World!</div>'
+        )
+        self.assertNotEqual(
+            div_node.to_html(), "<p class='test' id='test'>Hello, World!</p>"
+        )
+        self.assertEqual(
+            link_node.to_html(), '<a href="https://www.example.com">Click me!</a>'
+        )
+        self.assertNotEqual(div_node.to_html(), link_node.to_html())
+
+    def test_to_html_no_tag(self):
+        node = LeafNode(None, "Hello, World!")
+        self.assertEqual(node.to_html(), "Hello, World!")
