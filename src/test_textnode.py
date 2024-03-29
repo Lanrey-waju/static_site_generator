f8466ca551560ebf350d35729b035d4f11a8da81
Abdulmumin Akinde <lanreywaju97@gmail.com>
2024-03-29 21:31:43 +0100

    Wrote text_node to html_node logic and refactoed some tests

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,24 +1,32 @@
 import unittest
 
-from textnode import TextNode
+from textnode import (
+    TextNode,
+    text_type_text,
+    text_type_bold,
+    text_type_italics,
+    text_type_link,
+    text_type_code,
+    text_type_image,
+)
 
 
 class TestTextNode(unittest.TestCase):
     def test_equals(self):
-        node = TextNode("Hello, World!", "text")
-        node2 = TextNode("Hello, World!", "text")
+        node = TextNode("Hello, World!", text_type_text)
+        node2 = TextNode("Hello, World!", text_type_text)
         self.assertEqual(node, node2)
 
     def test_url_is_none(self):
-        node = TextNode("Hello, World!", "text")
+        node = TextNode("Hello, World!", text_type_text)
         self.assertEqual(node.url, None)
 
     def test_text_type_is_correct(self):
-        node = TextNode("Hello, World", "bold")
-        self.assertEqual(node.text_type, "bold")
+        node = TextNode("Hello, World", text_type_bold)
+        self.assertEqual(node.text_type, text_type_bold)
 
     def test_text_is_correct(self):
-        node = TextNode("Hello, World", "italics")
+        node = TextNode("Hello, World", text_type_italics)
         self.assertNotEqual(node.text, "Hello, World!")
         self.assertEqual(node.text, "Hello, World")
 
--- src/textnode.py
+++ src/textnode.py
@@ -1,3 +1,13 @@
+from htmlnode import LeafNode
+
+text_type_text = "text"
+text_type_bold = "bold"
+text_type_italics = "italics"
+text_type_link = "link"
+text_type_code = "code"
+text_type_image = "image"
+
+
 class TextNode:
     def __init__(self, text, text_type, url=None):
         self.text = text
@@ -16,3 +26,19 @@ class TextNode:
 
     def __repr__(self):
         return f"{TextNode}({self.text}, {self.text_type}, {self.url})"
+
+def text_node_to_html_node(text_node):
+    if text_node.text_type == text_type_text:
+        return LeafNode(text_node.text, tag=None")
+    elif text_node.text_type == text_type_bold:
+        return LeafNode(text_node.text, tag="b")
+    elif text_node.text_type == text_type_italics:
+        return LeafNode(text_node.text, tag="i")
+    elif text_node.text_type == text_type_link:
+        return LeafNode(text_node.text, tag="a", props={"href": text_node.url})
+    elif text_node.text_type == text_type_code:
+        return LeafNode(text_node.text, tag="code")
+    elif text_node.text_type == text_type_image:
+        return LeafNode("", tag="img", props={"src": text_node.url, "alt": text_node.text}))
+    else:
+        raise ValueError("Invalid text type: {textnode.text_type}")
