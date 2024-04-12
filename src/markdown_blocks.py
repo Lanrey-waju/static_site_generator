a0b62ffb2c683e3b434adaaaa8b0d7855c694176
Abdulmumin Akinde <lanreywaju97@gmail.com>
2024-04-12 23:58:59 +0100

    Implemented block to block types logic

--- src/markdown_blocks.py
+++ src/markdown_blocks.py
@@ -1,8 +1,23 @@
 block_type_paragraph = "paragraph"
+block_type_heading = "heading"
+block_type_code = "code"
+block_type_quote = "quote"
+block_type_unordered_list = "unordered_list"
+block_type_ordered_list = "ordered_list"
 
 
-def block_to_blocktype():
-    pass
+def block_to_block_type(markdown):
+    if markdown.startswith("# "):
+        return block_type_heading
+    if markdown.startswith("> "):
+        return block_type_quote
+    if markdown.startswith("```") and markdown.endswith("```"):
+        return block_type_code
+    if markdown.startswith("* "):
+        return block_type_unordered_list
+    if markdown.startswith("1. "):
+        return block_type_ordered_list
+    return block_type_paragraph
 
 
 def markdown_to_blocks(markdown_text):
