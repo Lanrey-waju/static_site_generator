30fe51e962f4ed068440b2356f2ce1b8fbdbc029
Abdulmumin Akinde <lanreywaju97@gmail.com>
2024-04-21 16:19:53 +0100

    Implemented recursive file copy from static to public

--- src/main.py
+++ src/main.py
@@ -1,9 +1,28 @@
-from textnode import TextNdoe
+import os
+import shutil
+
+
+source_path = "./static"
+destination_path = "./public"
 
 
 def main():
-    textnode = TextNdoe("Hello, World!", "text")
-    print(textnode)
+    recursive_copy(source_path, destination_path)
+
+
+def recursive_copy(source, destination):
+    if os.path.exists(destination):
+        shutil.rmtree(destination)
+    if not os.path.exists(destination):
+        os.mkdir(destination)
+    for item in os.listdir(source):
+        s = os.path.join(source, item)
+        d = os.path.join(destination, item)
+        print(f"from {s} to {d}")
+        if os.path.isdir(s):
+            recursive_copy(s, d)
+        else:
+            shutil.copy(s, d)
 
 
 main()
